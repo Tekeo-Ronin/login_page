91bd8887b553feea46c79d9f947c2612346e41cc
Volodymyr Korzhevskyi <korzhevskyiv@gmail.com>
2021-08-29 21:15:42 +0300

    Implementing the display of notifications

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,7 @@ import UI from './config/ui.config';
 import {validate} from './helpers/validate'
 import { showInputError, removeInputError } from './views/form';
 import { login } from './services/auth.service';
+import { notify } from './views/notifications';
 
 const {form, inputEmail, inputPassword} = UI;
 const inputs = [inputEmail, inputPassword];
